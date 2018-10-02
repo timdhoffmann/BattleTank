6dd8d2a7c772921d9089a3f8f582cb10e6a1da6a
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-10-02 18:13:55 +0200

    Implemented GetHitLocation(). HitLocation isn't updated correctly.

--- Source/S04_BattleTank/TankPlayerController.cpp
+++ Source/S04_BattleTank/TankPlayerController.cpp
@@ -7,18 +7,41 @@ void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto ControlledTank = GetControlledPawn();
+	const auto ControlledTank = GetControlledPawn();
 
 	if (ensureMsgf(ControlledTank != nullptr, TEXT("No controlled tank found.")))
 	{
 		UE_LOG(LogTemp, Warning, TEXT("[%s] found controlled pawn: %s"), *(GetName()), *(ControlledTank->GetName()));
 	}
+
 }
 
 // Called every frame to update this actor.
-void ATankPlayerController::Tick(float DeltaSeconds)
+void ATankPlayerController::Tick(const float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
+
+	AimAtCrosshair();
+}
+
+void ATankPlayerController::AimAtCrosshair() const
+{
+	if (!GetControlledPawn())
+	{
+		return;
+	}
+
+	auto OutHitLocation = FVector(0.0f, 0.0f, 0.0f); // Out parameter.
+	// Get world location from line trace through crosshair.
+	if (GetHitLocation(OutHitLocation))
+	{
+		
+		UE_LOG(LogTemp, Warning, TEXT("[%s] HitLocation: %s"), *(GetName()), *(OutHitLocation.ToString()));
+
+		// TODO: implementation:
+		// If it hits the landscape.
+		// Tell controlled tank to aim at this point. 
+	}
 }
 
 ATank* ATankPlayerController::GetControlledPawn() const
@@ -26,15 +49,48 @@ ATank* ATankPlayerController::GetControlledPawn() const
 	return Cast<ATank>(GetPawn());
 }
 
-void ATankPlayerController::AimAtCrosshair()
+bool ATankPlayerController::GetHitLocation(FVector OutHitLocation) const
 {
-	if (!GetControlledPawn())
-	{
-		return;
-	}
-	
-	// TODO: implementation.
-	// Get world location from linetrace through crosshair.
-	// If it hits the landscape.
-		// Tell controlled tank to aim at this point.
+	/// Set up parameters used for the LineTrace.
+	FHitResult OutHitResult;
+	const FCollisionObjectQueryParams ObjectTypesLookedFor(ECollisionChannel::ECC_PhysicsBody);
+	const FCollisionQueryParams AdditionalTraceParameters
+	(
+		FName(TEXT("")),	// use empty tag,
+		false,				// use simplified collider,
+		GetOwner()			// ignore ourselves (because the LineTrace starts in the center of our body).
+	);
+
+	// Execute LineTrace (Ray-cast).
+	return GetWorld()->LineTraceSingleByObjectType
+	(
+		OutHitResult, 
+		GetLineTraceStart(), 
+		GetLineTraceEnd(), 
+		ObjectTypesLookedFor, 
+		AdditionalTraceParameters
+	);
 }
+
+FVector ATankPlayerController::GetLineTraceStart() const
+{
+	// Get PlayerViewPoint data.
+	FVector OutLocation;
+	FRotator OutRotation;
+	GetPlayerViewPoint(OutLocation, OutRotation);
+
+	return OutLocation;
+}
+
+FVector ATankPlayerController::GetLineTraceEnd() const
+{
+	// Get PlayerViewPoint data.
+	FVector OutLocation;
+	FRotator OutRotation;
+	GetPlayerViewPoint(OUT OutLocation, OUT OutRotation);
+
+	const FVector LineTraceDirection = OutRotation.Vector(); // (Normalized) unit vector of length 1.
+	const int Meters = 100;
+	return OutLocation + LineTraceDirection * 10 * Meters;
+}
+
--- Source/S04_BattleTank/TankPlayerController.h
+++ Source/S04_BattleTank/TankPlayerController.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "Tank.h"
+#include "Camera/PlayerCameraManager.h"
 #include "CoreMinimal.h"
+#include "Engine/World.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h" // Must be the last include!
 
@@ -27,6 +29,14 @@ private:
 	/// Functions.
 	ATank* GetControlledPawn() const;
 
+	bool GetHitLocation(FVector OutHitLocation) const;
+
+	// Gets the start of the line trace.
+	FVector GetLineTraceStart() const;
+	
+	// Gets the end of the line trace.
+	FVector GetLineTraceEnd() const;
+
 	// Start the tank moving the barrel so that a shot would hit where the crosshair intersects the world.
-	void AimAtCrosshair();
+	void AimAtCrosshair() const;
 };
