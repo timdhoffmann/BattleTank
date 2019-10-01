2a9b0393b2b6041d37fd25c5c72350adfc43b183
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2019-10-01 22:45:19 +0200

    L234: CrossProduct() Vector Operator

--- Source/S04_BattleTank/Private/TankNavMovementComponent.cpp
+++ Source/S04_BattleTank/Private/TankNavMovementComponent.cpp
@@ -11,22 +11,31 @@ void UTankNavMovementComponent::RequestDirectMove(const FVector& MoveVelocity, b
 	// No need to call Super. Logic will be replaced.
 
 	const FVector TankForwardVectorNormal = GetOwner()->GetActorForwardVector();
-	// BUG: Why doesn't this get normalized?
+	// TODO: BUG! Why doesn't this get normalized?
 	const auto MoveVelocityNormal = MoveVelocity.GetSafeNormal(0.0001f);
+
+	// Calculates the ForwardThrow.
 	const float ForwardThrow = FVector::DotProduct(TankForwardVectorNormal, MoveVelocityNormal);
 
 	UE_LOG(LogTemp, Warning, TEXT("[%s] MoveVelocity: %s"), *GetOwner()->GetName(), *MoveVelocity.ToString());
-
 	UE_LOG(LogTemp, Warning, TEXT("[%s] TankForwardVectorNormal: %s, MoveVelocityNormal: %s, ForwardThrow: %f"),
 		*GetOwner()->GetName(),
 		*TankForwardVectorNormal.ToString(),
 		*MoveVelocityNormal.ToString(),
 		ForwardThrow);
-
 	DrawDebugLine(GetWorld(), GetOwner()->GetActorLocation(), GetOwner()->GetActorLocation() + MoveVelocityNormal, FColor::Magenta, false, -1, 0, 1);
 
-	// TODO: Remove clamping (only used due to the bug above.
-	IntendMoveForward(FMath::Clamp(ForwardThrow, 0.f, 1.f));
+	// TODO: Remove clamping (only used due to the bug above).
+	// Applies the ForwardThrow.
+	//IntendMoveForward(FMath::Clamp(ForwardThrow, -1.0f, 1.0f));
+
+	// Calculates the IntendTurnRight. Order of vectors matters for CrossProduct!
+	const float RightThrow = FVector::CrossProduct(TankForwardVectorNormal, MoveVelocityNormal).GetClampedToSize(-1.0f, 1.0f).Z;
+
+	// TODO: Remove clamping (only used due to the bug above).
+	// Applies the IntendTurnRight.
+	IntendTurnRight(RightThrow);
+	UE_LOG(LogTemp, Warning, TEXT("[%s] RightThrow: %f"), *GetOwner()->GetName(), RightThrow);
 }
 
 #pragma endregion
