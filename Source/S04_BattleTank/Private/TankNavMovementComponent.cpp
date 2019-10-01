9763cdbe7001f2a3744dfb0d8bdf1e12ebce306d
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2019-10-01 17:31:02 +0200

    L233: DotProduct() Vector Operator [BUG, see comments]

--- Source/S04_BattleTank/Private/TankNavMovementComponent.cpp
+++ Source/S04_BattleTank/Private/TankNavMovementComponent.cpp
@@ -7,39 +7,53 @@
 
 void UTankNavMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-    // No need to call Super. Logic will be replaced.
-    UE_LOG(LogTemp, Warning, TEXT("[%s] RequestDirectMove() velocity: %s"), *(GetOwner()->GetName()), *(MoveVelocity.ToString()));
+	// No need to call Super. Logic will be replaced.
+
+	const FVector TankForwardVectorNormal = GetOwner()->GetActorForwardVector();
+	// BUG: Why doesn't this get normalized?
+	const auto MoveVelocityNormal = MoveVelocity.GetSafeNormal(0.0001f);
+	const float ForwardThrow = FVector::DotProduct(TankForwardVectorNormal, MoveVelocityNormal);
+
+	UE_LOG(LogTemp, Warning, TEXT("[%s] MoveVelocity: %s"), *GetOwner()->GetName(), *MoveVelocity.ToString());
+
+	UE_LOG(LogTemp, Warning, TEXT("[%s] TankForwardVectorNormal: %s, MoveVelocityNormal: %s, ForwardThrow: %f"),
+		*GetOwner()->GetName(),
+		*TankForwardVectorNormal.ToString(),
+		*MoveVelocityNormal.ToString(),
+		ForwardThrow);
+
+	IntendMoveForward(ForwardThrow);
 }
 
 #pragma endregion
 
 void UTankNavMovementComponent::Init(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-    if (ensure(LeftTrackToSet != nullptr) && ensure(RightTrackToSet != nullptr))
-    {
-        LeftTrack = LeftTrackToSet;
-        RightTrack = RightTrackToSet;
-    }
+	if (ensure(LeftTrackToSet != nullptr) && ensure(RightTrackToSet != nullptr))
+	{
+		LeftTrack = LeftTrackToSet;
+		RightTrack = RightTrackToSet;
+	}
 }
 
 void UTankNavMovementComponent::IntendMoveForward(const float Throw) const
 {
-    // TODO: Prevent double-speed when using multiple input devices.
+	// TODO: Prevent double-speed when using multiple input devices.
 
-    if (ensure(LeftTrack != nullptr && RightTrack != nullptr))
-    {
-        LeftTrack->SetThrottle(Throw);
-        RightTrack->SetThrottle(Throw);
-    }
+	if (ensure(LeftTrack != nullptr && RightTrack != nullptr))
+	{
+		LeftTrack->SetThrottle(Throw);
+		RightTrack->SetThrottle(Throw);
+	}
 }
 
 void UTankNavMovementComponent::IntendTurnRight(const float Throw) const
 {
-    // TODO: Prevent double-speed when using multiple input devices.
+	// TODO: Prevent double-speed when using multiple input devices.
 
-    if (ensure(LeftTrack != nullptr && RightTrack != nullptr))
-    {
-        LeftTrack->SetThrottle(Throw);
-        RightTrack->SetThrottle(-Throw);
-    }
+	if (ensure(LeftTrack != nullptr && RightTrack != nullptr))
+	{
+		LeftTrack->SetThrottle(Throw);
+		RightTrack->SetThrottle(-Throw);
+	}
 }
