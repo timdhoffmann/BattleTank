a995e907796f35f934e837b0925aeb9a874e75a6
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2019-10-05 12:07:21 +0200

    L248: Finishing our Refactoring

--- Source/S04_BattleTank/Private/Tank.cpp
+++ Source/S04_BattleTank/Private/Tank.cpp
@@ -1,7 +1,6 @@
 // Copyright Tim Hoffmann (@timdhoffmann).
 
 #include "Tank.h"
-#include "Projectile.h"
 
 // Sets default values
 ATank::ATank()
@@ -9,13 +8,3 @@ ATank::ATank()
 	//Doesn't need to tick every frame.
 	PrimaryActorTick.bCanEverTick = false;
 }
-
-#pragma region Overrides
-
-// Called when the game starts or when spawned
-void ATank::BeginPlay()
-{
-	Super::BeginPlay();
-}
-
-#pragma endregion
--- Source/S04_BattleTank/Public/Tank.h
+++ Source/S04_BattleTank/Public/Tank.h
@@ -12,25 +12,12 @@ class S04_BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-#pragma region Variables
-
-public:
-
-	// TODO: Remove once firing is moved to aiming component.
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Firing)
-		float LaunchSpeed = 4e3f;
-
-#pragma endregion
-
 #pragma region Functions
 
 private:
 
 	// Sets default values for this pawn's properties. Can be private in UE4!
 	ATank();
-
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
 #pragma endregion
 };
